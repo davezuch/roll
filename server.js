6ed5820a4a40a416162aa8f92f0154137f5a87a4
David <dzuch@citizennet.com>
2015-08-19 18:56:11 -0700

    updated to use default port 80

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // BASE SETUP
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
 
 // ROUTES
 var router = express.Router();
